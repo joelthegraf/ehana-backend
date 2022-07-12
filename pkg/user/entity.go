0f6a7c83f1e9ecc0da56e5147c79c3661f8ec5cf
Joel Graf <62430653+joelthegraf@users.noreply.github.com>
2022-07-12 11:41:31 +0200

    docs: add information about unique user fields

--- pkg/user/entity.go
+++ pkg/user/entity.go
@@ -4,9 +4,9 @@ import "github.com/google/uuid"
 
 // A User represents a ehana account.
 type User struct {
-	ID       uint64
-	UUID     uuid.UUID
-	Username string
+	ID       uint64    // ID has to be unique.
+	UUID     uuid.UUID // UUID has to be unique.
+	Username string    // Username has to be unique.
 	Password string
 	Created  int64
 	Modified int64
